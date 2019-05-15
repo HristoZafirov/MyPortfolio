36484108bd10229c954d2d9222f40846b644ed28
Hristo Zafirov <hristozafirov96@gmail.com>
2019-05-15 18:48:00 +0300

    Added parallax, improved portfolio layer

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -41,3 +41,43 @@ function toggleModalOff() {
 	icon.setAttribute("onclick", "toggleModalOn()");
 }
 
+
+function parallax(selector, context) {
+    context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+}
+// TO BE FIXED !!!
+function fadeIn(par, par2){
+    var scrolledHeight= window.pageYOffset;
+    var param = document.getElementById(par);
+    var limit = document.getElementById(par2);
+    var p1 = document.getElementById('demo2');
+    var calc = document.getElementById('demo3');
+    p2.innerHTML = limit.offsetTop;
+    calc.innerHTML = calcHeight(scrolledHeight);
+        if(scrolledHeight >= (screen.height * calcHeight(scrolledHeight) - (Math.abs(limit.offsetTop) * (calcHeight(scrolledHeight)+1)))) {
+            param.style.display = "block";
+        }
+}
+
+window.addEventListener("scroll", function() {
+    var scrolledHeight= window.pageYOffset;
+
+    parallax(".parallax").forEach(function(par,index,array) {
+
+        var limit= par.offsetTop + par.offsetHeight;
+
+        if(scrolledHeight > par.offsetTop && scrolledHeight <= limit) {
+            par.style.backgroundPositionY= (scrolledHeight - par.offsetTop) /2.5+ "px";
+        } 
+        else {
+            par.style.backgroundPositionY= "0";
+        }
+    });
+    if (calcHeight(scrolledHeight) == 1 ) {
+        fadeIn('fadeIn1', 'boxfade1');
+    } else if (calcHeight(scrolledHeight) == 2) {
+        fadeIn('fadeIn2', 'boxfade2');
+    }
+});
